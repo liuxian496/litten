188229c7935b48d4cdb2be0cd8e1fa995f165e06
liuxian496 <liuxian494@163.com>
2023-07-30 11:50:16 +0800

    docs: 完善iconButton控件注释

--- src/components/iconButton/iconButton.types.ts
+++ src/components/iconButton/iconButton.types.ts
@@ -3,5 +3,8 @@ import { RippleColor } from '../ripple/ripple.types';
 
 export interface IconButtonProps extends ButtonBaseProps {
     'aria-label'?: string,
+    /**
+     * 设置ripple组件的颜色
+     */
     rippleColor?: RippleColor;
 }
