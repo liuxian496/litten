ca67acf28e552e493932166d035cebfaacd15748
liuxian496 <liuxian494@163.com>
2023-12-11 12:27:27 +0800

    fix: 添加getDefaultValueByDisplayName，修改form获取defaultValue不正确的问题

--- src/components/control/contentControl.tsx
+++ src/components/control/contentControl.tsx
@@ -7,6 +7,28 @@ import {
 } from "react";
 import { ContentControlProps } from "./control.types";
 import { usePrevious } from "../../global/util";
+import { UserControlType } from "../../global/enum";
+
+/**
+ * 检测一个数是否在给定的极值范围之内，并返回检测后的数值。如果小于最小值返回最小值，如果大于最大值返回最大值
+ * @param value 待检测的数值
+ * @param extremum 极值范围
+ * @returns 检测后校准的数值
+ */
+// export function checkExtremum(value: number, extremum: Extremum) {
+//     const { min, max } = extremum;
+//     let result;
+
+//     if (value >= min && value <= max) {
+//         result = value;
+//     } else if (value < min) {
+//         result = min;
+//     } else if (value > max) {
+//         result = max;
+//     }
+
+//     return result;
+// }
 
 /**
  * value值不是undefined时，返回value值。否则返回defaultValue的值
@@ -26,6 +48,25 @@ export function getCurrentValue<V>(
     }
 
     return current;
+}
+
+/**
+ * 如果没有设置DefaultValue，原生控件会有默认值。根据Litten控件displayName获取这个默认值
+ * @param displayName Litten控件名称
+ */
+export function getDefaultValueByDisplayName(displayName: string) {
+    let defaultValue;
+
+    switch (displayName) {
+        case UserControlType.Slider:
+            defaultValue = 50;
+            break;
+        default:
+            defaultValue = "";
+            break;
+    }
+
+    return defaultValue;
 }
 
 /**
