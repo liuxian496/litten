74fad6d2bc845c6c51fb071e8572b0b5cd40232e
liuxian496 <liuxian494@163.com>
2024-04-25 19:07:50 +0800

    refactor: 将getPrefixNs、isEmptyString、printArrayItem抽取到cyndi

--- src/stories/util.stories.tsx
+++ src/stories/util.stories.tsx
@@ -1,7 +1,5 @@
 import { Meta, StoryObj } from "@storybook/react";
 
-import { GetPrefixNsTest } from "../test/util/getPrefixNsTest";
-import { PrintArrayItemTest } from "../test/util/printArrayItemTest";
 import { StopPropagationTest } from "../test/util/stopPropagationTest";
 import { UsePreviousTest } from "../test/util/usePreviousTest";
 import { UseRelativePositionTest } from "../test/util/useRelativePositionTest";
@@ -11,16 +9,6 @@ export default {
 } as Meta;
 
 export type UtilStory = StoryObj;
-
-export const GetPrefixNs = {
-  name: "getPrefixNs",
-  ...GetPrefixNsTest,
-};
-
-export const PrintArrayItem = {
-  name: "printArrayItem",
-  ...PrintArrayItemTest,
-};
 
 export const StopPropagation = {
   name: "stopPropagation",
